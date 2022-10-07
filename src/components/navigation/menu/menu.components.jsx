f3447f35acac976ca30ee344f9fbd8f6f457ba83
GomezFrannco <frangomez.baires@gmail.com>
2022-10-07 02:03:05 -0300

    [Add] Nav menu component created

---
+++ src/components/navigation/menu/menu.components.jsx
@@ -0,0 +1,18 @@
+const NavMenu = () => {
+  <ul className="menu__list">
+    <li className="list__option">
+      <a href='#' className="option__text">Projects</a>
+    </li>
+    <li className="list__option">
+      <a href='#' className="option__text">Experience</a>
+    </li>
+    <li className="list__option">
+      <a href='#' className="option__text">Skills</a>
+    </li>
+    <li className="list__option">
+      <a href='#' className="option__text">About me</a>
+    </li>
+  </ul>;
+};
+
+export default NavMenu;
