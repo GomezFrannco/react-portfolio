c95a503e5cd8688ef230aa63417975cce9572419
GomezFrannco <frangomez.baires@gmail.com>
2022-10-07 02:07:04 -0300

    [Add] Header component created

---
+++ src/layout/header/header.layout.jsx
@@ -0,0 +1,25 @@
+import Button from "../../components/buttons/button.components";
+import NavBar from "../../components/navigation/navBar.components";
+
+
+export default function Header() {
+  return (
+    <>
+      <header className="layout__header">
+        <NavBar/>
+        <div className="header__content">
+          <div className="content__text">
+            <h1>Backend Developer</h1>
+            <span>TypeScript.</span>
+            <p>I create solutions by writing code as efficiently as possible.</p>
+            <p>We can build amazing and innovative things.</p>
+            <Button btnName="Download CV"/>
+          </div>
+          <div className="content__logo">
+            <img src="" alt=""/>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+};
