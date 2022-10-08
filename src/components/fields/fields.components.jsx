fa70b5a58fe3ba03e7a6b20b0628b7e004900e24
GomezFrannco <frangomez.baires@gmail.com>
2022-10-08 18:12:22 -0300

    [Add] Inputs have been created

---
+++ src/components/fields/fields.components.jsx
@@ -0,0 +1,15 @@
+export const TextInput = () => {
+  return (
+    <input type='text' placeholder='' />
+  );
+};
+export const EmailInput = () => {
+  return (
+    <input type='email' placeholder='Type your email'/>
+  );
+};
+export const TextAreaInput = () => {
+  return (
+    <textarea name='' id='' cols='30' rows='10' placeholder='Write a short message'></textarea>
+  );
+};
