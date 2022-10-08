721646616460452b44d5d2a0b0249bf41c989066
GomezFrannco <frangomez.baires@gmail.com>
2022-10-08 18:21:32 -0300

    [Add] Card widget was created

---
+++ src/components/cards/projects.components.jsx
@@ -0,0 +1,14 @@
+const Project = ({project}) => {
+  return (
+    <>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Project;
