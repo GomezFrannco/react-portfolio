ddbca9efba59836f79b179fb3b20697a7373b881
GomezFrannco <frangomez.baires@gmail.com>
2022-10-14 16:22:46 -0300

    [Fix] Component return have been fixed

--- src/components/navigation/menu/menu.components.jsx
+++ src/components/navigation/menu/menu.components.jsx
@@ -1,18 +1,22 @@
+import "./menu.components.scss";
+
 const NavMenu = () => {
-  <ul className="menu__list">
-    <li className="list__option">
-      <a href='#' className="option__text">Projects</a>
-    </li>
-    <li className="list__option">
-      <a href='#' className="option__text">Experience</a>
-    </li>
-    <li className="list__option">
-      <a href='#' className="option__text">Skills</a>
-    </li>
-    <li className="list__option">
-      <a href='#' className="option__text">About me</a>
-    </li>
-  </ul>;
+  return (
+    <ul className="menu__list">
+      <li className="list__option">
+        <a href='#' className="option__text">Projects</a>
+      </li>
+      <li className="list__option">
+        <a href='#' className="option__text">Experience</a>
+      </li>
+      <li className="list__option">
+        <a href='#' className="option__text">Skills</a>
+      </li>
+      <li className="list__option">
+        <a href='#' className="option__text">About me</a>
+      </li>
+    </ul>
+  )
 };
 
 export default NavMenu;
