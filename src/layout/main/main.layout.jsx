25214999f23fe30dd175a01765179d56aebfd79f
GomezFrannco <frangomez.baires@gmail.com>
2022-10-20 14:33:45 -0300

    Add text to project section

--- src/layout/main/main.layout.jsx
+++ src/layout/main/main.layout.jsx
@@ -1,7 +1,21 @@
+import Button from "../../components/buttons/button.components";
+import { Subtitles } from "../../components/text/titles.components";
+
 export default function Main() {
   return (
     <main className='layout__main'>
       <section className='main__section' id='mainProjects'>
+        <div className='section__content'>
+          <Subtitles text='Latest Projects' />
+          <p>
+            I've been involved in some interesting work. Let me show you three
+            amazing projects and the kind of things I can build.
+          </p>
+        </div>
+        <div className='section__content'>
+          <Subtitles text='More Projects' />
+          <Button btnName='View all projects' />
+        </div>
       </section>
       <section className='main__section' id='mainExperience'>
       </section>
