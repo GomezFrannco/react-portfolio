90d3e8a803bd5422c1ca25f7bfb5a01d32265ed2
GomezFrannco <frangomez.baires@gmail.com>
2022-10-20 14:26:25 -0300

    Add Main component

---
+++ src/layout/main/main.layout.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main className='layout__main'>
+      <section className='main__section' id='mainProjects'>
+      </section>
+      <section className='main__section' id='mainExperience'>
+      </section>
+      <section className='main__section' id='mainSkills'>
+      </section>
+      <section className='main__section' id='mainAbout'>
+      </section>
+    </main>
+  );
+}
