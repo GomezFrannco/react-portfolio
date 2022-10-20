6f1c95de7d6eb3d9fb2df907eced6ee73d820f43
GomezFrannco <frangomez.baires@gmail.com>
2022-10-20 18:31:24 -0300

    Add project card button

--- src/components/cards/projects.components.jsx
+++ src/components/cards/projects.components.jsx
@@ -1,3 +1,4 @@
+import Button from "../buttons/button.components";
 import "./project.components.scss";
 
 const Project = ({project}) => {
@@ -5,6 +6,9 @@ const Project = ({project}) => {
     <>
       <div className="component__card">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9KfnymzaK4rOAK8GT3uC9V1P4PYQvsg5FVw&usqp=CAU" alt="" className="card__image"/>
+        <div className="card__button">
+          <Button btnName="See project"/>
+        </div>
         <div className="card__text">
           <h3>{project.title}</h3>
           <p>{project.description}</p>
