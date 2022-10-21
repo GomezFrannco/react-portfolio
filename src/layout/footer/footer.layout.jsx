7ba2f1815082d0920c10017c687ebc22fb552743
GomezFrannco <frangomez.baires@gmail.com>
2022-10-21 18:37:07 -0300

    Add Footer contact form

--- src/layout/footer/footer.layout.jsx
+++ src/layout/footer/footer.layout.jsx
@@ -1,3 +1,4 @@
+import { EmailInput, TextAreaInput, TextInput } from "../../components/fields/fields.components";
 import { Subtitles } from "../../components/text/titles.components";
 
 export default function Footer() {
@@ -9,6 +10,16 @@ export default function Footer() {
           <p>
             I'm willing to <span>work with you</span> so if you think <span>I fit</span> your <span>projects requirements</span> feel free to <span>contact me</span> through my social networks or email.
           </p>
+        </div>
+        <div className="footer__form">
+          <form action="">
+            <div>
+              <TextInput placeholder="Type your name"/>
+              <EmailInput placeholder="Type your email"/>
+            </div>
+            <TextInput placeholder="Topic of your message"/>
+            <TextAreaInput/>
+          </form>
         </div>
       </footer>
     </>
