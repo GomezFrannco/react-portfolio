91ef790622a1027a99c0d0e0d3c702c05edfd00e
GomezFrannco <frangomez.baires@gmail.com>
2022-10-21 18:43:05 -0300

    Add skills object to export

--- src/components/skill/skill.components.jsx
+++ src/components/skill/skill.components.jsx
@@ -1,3 +1,15 @@
+import docker from "../../assets/skills/skill_docker.png";
+import python from "../../assets/skills/skill_python.png";
+import node from "../../assets/skills/skill_node.png";
+import graphql from "../../assets/skills/skill_graphql.png";
+import php from "../../assets/skills/skill_php.png";
+import mongo from "../../assets/skills/skill_mongo.png";
+import postgresql from "../../assets/skills/skill_postgresql.png";
+import mysql from "../../assets/skills/skill_mysql.png";
+import typescript from "../../assets/skills/skill_typescript.png";
+import javascript from "../../assets/skills/skill_javascript.png";
+import git from "../../assets/skills/skill_git.png";
+
 export default function SkillImage({skill, skillName}) {
   return (
     <>
@@ -7,4 +19,18 @@ export default function SkillImage({skill, skillName}) {
       </div>
     </>
   )
+}
+
+export const skills = {
+  docker,
+  python,
+  node,
+  graphql,
+  php,
+  mongo,
+  postgresql,
+  mysql,
+  typescript,
+  javascript,
+  git,
 }
