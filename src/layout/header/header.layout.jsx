544e378e3645f0a2b06c00a76e7fcda4e9c654f5
GomezFrannco <frangomez.baires@gmail.com>
2022-10-22 18:52:35 -0300

    Add an interval to change the languages text

--- src/layout/header/header.layout.jsx
+++ src/layout/header/header.layout.jsx
@@ -3,10 +3,20 @@ import NavBar from "../../components/navigation/navBar.components";
 import "./header.layout.scss";
 import logo from "../../assets/main-logo.svg";
 import { langs } from "./langs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const [lang, setLang] = useState(langs[0]);
+  const [index, setIndex] = useState(1);
+  useEffect(()=> {
+    const interval = setInterval(()=>{
+      setLang(langs[index])
+      index != langs.length - 1
+        ? setIndex(prevIndex => prevIndex + 1)
+        : setIndex(0);
+    }, 1100)
+    return () => clearInterval(interval);
+  })
   return (
     <>
       <header className="layout__header">
