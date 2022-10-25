0d11f89c0954f9681f5db3bc328e6c9f0d547f9a
GomezFrannco <frangomez.baires@gmail.com>
2022-10-25 13:37:16 -0300

    Add About Section as a component with motion

---
+++ src/layout/main/sections/about.sections.jsx
@@ -0,0 +1,51 @@
+import { MotionSubtitle } from "../../../components/text/titles.components";
+import { MotionButton } from "../../../components/buttons/button.components";
+import { motion } from "framer-motion";
+import "../main.layout.scss";
+
+const initialValue = {
+  opacity: 0,
+  y: 50,
+};
+const scrollAnimation = {
+  opacity: 1,
+  y: 0,
+};
+const transitionTime = {
+  duration: 0.6,
+};
+
+export default function About() {
+  return (
+    <section className='main__section' id='about'>
+      <div className='section__text'>
+        <MotionSubtitle
+          text='About me'
+          initial={{ y: -50, opacity: 0 }}
+          whileInView={scrollAnimation}
+          transition={transitionTime}
+        />
+        <motion.p
+          initial={initialValue}
+          whileInView={scrollAnimation}
+          transition={transitionTime}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua odio.
+          Morbi.
+        </motion.p>
+        <MotionButton
+          btnName='See more'
+          initial={initialValue}
+          whileInView={scrollAnimation}
+          transition={transitionTime}
+        />
+      </div>
+      <img
+        src='https://i.ytimg.com/vi/n6JyVpU89mo/maxresdefault.jpg'
+        alt=''
+        className='section__image'
+      />
+    </section>
+  );
+}
