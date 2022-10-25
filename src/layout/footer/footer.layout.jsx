85a1875cf8726fec103c12a1330e7429f5468d6b
GomezFrannco <frangomez.baires@gmail.com>
2022-10-25 13:46:18 -0300

    Add motion animation

--- src/layout/footer/footer.layout.jsx
+++ src/layout/footer/footer.layout.jsx
@@ -1,18 +1,41 @@
 import { EmailInput, TextAreaInput, TextInput } from "../../components/fields/fields.components";
 import { Subtitles } from "../../components/text/titles.components";
+import {motion} from "framer-motion";
 import "./footer.layout.scss";
+
+const scrollAnimation = {
+  x: 0,
+  opacity: 1
+}
+const transitionTime = {
+  duration: 0.6
+}
 
 export default function Footer() {
   return (
     <>
       <footer className="layout__footer">
-        <div className="footer__text">
+        <motion.div className="footer__text"
+          initial={{
+            x: -150,
+            opacity: 0
+          }}
+          whileInView={scrollAnimation}
+          transition={transitionTime}
+        >
           <Subtitles text="Let's work together"/>
           <p>
             I'm willing to <span>work with you</span> so if you think <span>I fit</span> your <span>projects requirements</span> feel free to <span>contact me</span> through my social networks or email.
           </p>
-        </div>
-        <div className="footer__form">
+        </motion.div>
+        <motion.div className="footer__form"
+          initial={{
+            x: 150,
+            opacity: 0
+          }}
+          whileInView={scrollAnimation}
+          transition={transitionTime}
+        >
           <form action="">
             <div>
               <TextInput placeholder="Type your name"/>
@@ -25,7 +48,7 @@ export default function Footer() {
           <div className="form__social">
             <p>Follow me on social media:</p>
           </div>
-        </div>
+        </motion.div>
       </footer>
     </>
   )
