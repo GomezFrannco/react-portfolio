ef44dc124eb20160b0ba6fff67ec68ae31ccccb2
GomezFrannco <frangomez.baires@gmail.com>
2022-10-24 22:12:24 -0300

    Add a component with motion library

--- src/components/text/titles.components.jsx
+++ src/components/text/titles.components.jsx
@@ -1,9 +1,13 @@
+import { forwardRef } from "react";
+import { motion } from "framer-motion";
 import "./subtitles.components.scss";
 
-export const Subtitles = ({text}) => {
+export const Subtitles = forwardRef(({text},ref) => {
   return (
-    <div className="subtitles">
+    <div className="subtitles" ref={ref}>
       <h2>{text}</h2>
     </div>
   );
-};
+});
+
+export const MotionSubtitle = motion(Subtitles);
