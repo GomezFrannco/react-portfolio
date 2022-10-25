3327b9c07584b104deaa81a5548f42f4fea8a53a
GomezFrannco <frangomez.baires@gmail.com>
2022-10-25 13:36:42 -0300

    Add Experience Section as a component with motion

---
+++ src/layout/main/sections/experience.sections.jsx
@@ -0,0 +1,68 @@
+import { Subtitles } from "../../../components/text/titles.components";
+import { MotionButton } from "../../../components/buttons/button.components";
+import { motion } from "framer-motion";
+import "../main.layout.scss";
+
+const scrollAnimation = {
+  opacity: 1,
+  y: 0,
+};
+const scrollViewport = {
+  once: true,
+  amount: "all",
+};
+const transitionTime = {
+  duration: 0.6,
+};
+
+export default function Experience() {
+  return (
+    <section className='main__section' id='experience'>
+      <div className='section__content'>
+        <motion.div
+          className='content__title'
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={scrollAnimation}
+          viewport={scrollViewport}
+          transition={transitionTime}
+        >
+          <Subtitles text='Experience' />
+          <div className='title__introduction'>
+            <p>
+              I have worked in different projects and companies as a developer.
+            </p>
+            <p>You can see my professional background here.</p>
+          </div>
+        </motion.div>
+        <motion.div
+          className='content__experience'
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={scrollAnimation}
+          viewport={scrollViewport}
+          transition={{ duration: 0.6, delay: 0.2 }}
+        >
+          <div className='experience__timeline'>
+            <div className='timeline__events'></div>
+          </div>
+          <div className='experience__text'>
+            <h3>Backend Developer</h3>
+            <p>
+              <i>Mercado Libre • 06/07/2020 - 07/01/2021</i>
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque,
+              hic eligendi error minus asperiores voluptas
+            </p>
+          </div>
+        </motion.div>
+        <MotionButton
+          btnName='LinkedIn Profile'
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={scrollAnimation}
+          viewport={scrollViewport}
+          transition={transitionTime}
+        />
+      </div>
+    </section>
+  );
+}
