107e77c3f5c2e37b8ebc6d6865d465c7d8b85581
GomezFrannco <frangomez.baires@gmail.com>
2022-11-01 20:16:34 -0300

    Add the layout to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import Button from "./components/buttons/button.components";
+import Header from "./layout/header/header.layout";
+import Main from "./layout/main/main.layout";
+import Footer from "./layout/footer/footer.layout";
 
 export default function App() {
   return (
     <>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   );
 }
