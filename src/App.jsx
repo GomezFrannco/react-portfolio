9e1dc8dcbd801917e88b015984b438c4483fb5ce
GomezFrannco <frangomez.baires@gmail.com>
2022-11-16 23:15:10 -0300

    Add routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import Header from "./layout/header/header.layout";
 import Main from "./layout/main/main.layout";
 import Footer from "./layout/footer/footer.layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProjectsPage from "./pages/projects";
 
 export default function App() {
   return (
-    <>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header/>
+            <Main/>
+            <Footer/>
+          </>
+        }/>
+        <Route path="/projects" element={
+          <ProjectsPage/>
+        }/>
+      </Routes>
+    </BrowserRouter>
   );
 }
