a7604b1d55ecfe7bd21f93f4cec68c80879236a2
GomezFrannco <frangomez.baires@gmail.com>
2023-01-04 21:15:54 -0300

    Remove the old input components

--- src/components/fields/fields.components.jsx
+++ src/components/fields/fields.components.jsx
@@ -1,16 +1,6 @@
 import { useState } from "react";
 import "./fields.components.scss";
 
-export const TextInput = ({placeholder}) => {
-  return (
-    <input className="input__text" type='text' placeholder={placeholder} required/>
-  );
-};
-export const EmailInput = ({placeholder}) => {
-  return (
-    <input type='email' placeholder={placeholder} required/>
-  );
-};
 export const TextAreaInput = () => {
   return (
     <textarea name='' id='' cols='30' rows='10' placeholder='Write a short message'></textarea>
