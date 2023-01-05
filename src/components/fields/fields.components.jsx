2f83ec38c11743694f280f6cbd4e0b9a8476b721
GomezFrannco <frangomez.baires@gmail.com>
2023-01-04 21:08:41 -0300

    Add a better input component

--- src/components/fields/fields.components.jsx
+++ src/components/fields/fields.components.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./fields.components.scss";
 
 export const TextInput = ({placeholder}) => {
@@ -13,5 +14,20 @@ export const EmailInput = ({placeholder}) => {
 export const TextAreaInput = () => {
   return (
     <textarea name='' id='' cols='30' rows='10' placeholder='Write a short message'></textarea>
+  );
+};
+
+export const Input = ({ type, id, name, ph }) => {
+  const [placeholder, setPlaceholder] = useState(ph);
+
+  return (
+    <input
+      type={type}
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      onFocus={() => setPlaceholder("")}
+      onBlur={() => setPlaceholder(ph)}
+    />
   );
 };
